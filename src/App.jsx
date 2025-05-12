99c68b9b246317db74342a4bd65d3d957f1a92cf
Arshdeep Singh <singharshdeep9039@gmail.com>
2025-05-13 00:01:03 +0530

    height issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -832,7 +832,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div
-      className="min-w-screen bg-cover bg-center max-h-[100vh] overflow-hidden"
+      className="min-w-screen bg-cover bg-center max-h-[100vh] overflow-scroll"
       style={{ backgroundImage: "url('/Background.jpg')" }}
     >
       <Navbar />
